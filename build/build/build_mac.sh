2b221a566ed3c51811431e2a30153cf6680600e2
Archana Radhakrishnan <>
2018-10-23 23:13:37 -0400

    Canon moms for current

--- build/build/build_mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-../configure --prefix="${HOME}/Kinematic_factor/compute_Kfactors_install" \
-             --with-adat="${HOME}/Kinematic_factor/adat_devel_install" \
-             CXXFLAGS="-O3 -mtune=native -fopenmp -fbounds-check" CFLAGS="-O3 -mtune=native -fopenmp -fbounds-check" \
-             CXX=g++-8 CC=gcc-8
